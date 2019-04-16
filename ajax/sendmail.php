a6b1da4b210132fc47f27eebfc07748d434529e3
Sheyilaaw <sheyilaaw98@gmail.com>
2019-04-16 23:49:25 +0100

    Added php code to send mail from contact form

---
+++ ajax/sendmail.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+
+$request = $_POST;
+$required = array('name', 'email', 'subject' , 'message');
+$response = [];
+
+if(!$request) {
+    $response = ['code' => 2, 'message' => 'Kindly fill all input fields'];
+    die();
+}
+
+if (count(array_intersect_key(array_flip($required), $request)) !== count($required)) {
+    $response = [ 'code' => 2, 'message' => 'Kindly fill in all inputs'];
+    die();
+}
+
+//Validate Form
+require_once "../libs/functions.php";
+
+$errors = validateContactForm($request);
+if($errors !== null) {
+    $response = ['code' => 2, 'message' => $errors];
+    echo json_encode($response);
+    die();
+}
+
+$mailResponse = sendMail($request);
+if($mailResponse !== 'Message has been sent') {
+    $response = [ 'code' => 3, 'message' => $mailResponse ];
+    die();
+}
+
+$response = [ 'code' => 1, 'message' => $mailResponse];
+echo json_encode($response);
+die();
---
+++ libs/functions.php
@@ -0,0 +1,64 @@
+<?php
+
+function sendMail($request): string {
+    require "PHPMailer/PHPMailer.php";
+    require "PHPMailer/SMTP.php";
+
+    $mailHost = 'mail.asyncdeveloper.com';
+    $mailName = 'Oluwaseyi Adeogun';
+    $mailUsername = 'contact@asyncdeveloper.com';
+    $mailPassword = 'nbvhgfytrm98';
+
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->isSMTP();                             // Set mailer to use SMTP
+        $mail->Host       = $mailHost;              // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                   // Enable SMTP authentication
+        $mail->Username   = $mailUsername;          // SMTP username
+        $mail->Password   = $mailPassword;          // SMTP password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+
+        //Recipients
+        $mail->setFrom($request['email'], $request['name']);
+        $mail->addAddress($mailUsername, $mailName);
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $request['subject'];
+        $mail->Body    = $request['message'];
+        $mail->AltBody = $request['message'];
+        $mail->send();
+        return 'Message has been sent';
+    } catch (Exception $e) {
+        return "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+function validateContactForm($request) {
+    $errors = null ;
+
+    if(strlen($request['name']) === 0) {
+        $errors.="Name is required <br>";
+    }
+
+    if(strlen($request['email']) > 0) {
+        if(!filter_var($request['email'],FILTER_VALIDATE_EMAIL)) {
+            $errors.="Invalid email address <br>";
+        }
+    }else {
+        $errors.="Email address is required <br>";
+    }
+
+    if(strlen($request['subject']) === 0) {
+        $errors.="Subject is required <br>";
+    }
+
+    if(strlen($request['message']) === 0) {
+        $errors.="Message is required <br>";
+    }
+
+    return $errors;
+}
