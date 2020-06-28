cca2c63bd5e17cd67478258a33da212e2ff4772a
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-06-28 18:44:49 +0100

    Enable copy and paste on page

--- js/custom.js
+++ js/custom.js
@@ -43,19 +43,7 @@ $("#status").fadeOut(), $("#preloader").delay(350).fadeOut("slow"), $("body").de
         typeSpeed: 100,
         backDelay: 3e3
     })
-}), $("body").bind("cut copy paste", function(e) {
-    e.preventDefault()
-}), window.onload = function() {
-    function e(e) {
-        return e.stopPropagation ? e.stopPropagation() : window.event && (window.event.cancelBubble = !0), e.preventDefault(), !1
-    }
-    document.addEventListener("contextmenu", function(e) {
-        e.preventDefault()
-    }, !1), document.addEventListener("keydown", function(t) {
-        t.ctrlKey && t.shiftKey && 73 == t.keyCode && e(t), t.ctrlKey && t.shiftKey && 74 == t.keyCode && e(t), 83 == t.keyCode && (navigator.platform.match("Mac") ? t.metaKey : t.ctrlKey) && e(t), t.ctrlKey && 85 == t.keyCode && e(t), 123 == event.keyCode && e(t)
-    }, !1)
-};
-$(".element").each(function() {
+}), $(".element").each(function() {
     var $this = $(this);
     $this.typed({
         strings: $this.attr('data-elements').split(','),
